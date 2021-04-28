44091d877243de68fae0b2e0e12dcc3ab1bd2dbb
Eva <yi.zheng1709@gmail.com>
2021-04-28 19:54:20 -0400

    adds universal.css to hold the classes that are used more than two times

--- frontend/index.js
+++ frontend/index.js
@@ -1,12 +1,24 @@
 const initialButton = document.getElementById("initial-button")
 const initialContact = document.getElementById("initial-contact")
+const findOrCreateProject = document.getElementById("find-or-create-project")
+const createNewProjectButton = document.getElementById("create-new-project-button")
+const findProjectButton = document.getElementById("find-project-button")
 const findProject = document.getElementById("find-project")
 const logo = document.getElementById("logo")
 
 
 function deleteInitialAndContinue() {
     initialContact.style.display = "none"
-    //show search project
+    findOrCreateProject.style.display = "block"
+}
+
+function newProjectForm() {
+    findOrCreateProject.style.display = "none"
+    
+}
+
+function findProjectForm() {
+    findOrCreateProject.style.display = "none"
     findProject.style.display = "block"
 }
 
@@ -17,3 +29,5 @@ function goBackToInitial() {
 
 initialButton.addEventListener("click", deleteInitialAndContinue)
 logo.addEventListener("click", goBackToInitial)
+findProjectButton.addEventListener("click", findProjectForm)
+createNewProjectButton.addEventListener("click", newProjectForm)
