de23291819856b2cbb70bf006d6e62a6a02c07f3
Eva <yi.zheng1709@gmail.com>
2021-04-28 19:01:44 -0400

    imports fridaybanner.gif and adds it to img tag under banner-div

--- frontend/index.js
+++ frontend/index.js
@@ -1,12 +1,19 @@
 const initialButton = document.getElementById("initial-button")
 const initialContact = document.getElementById("initial-contact")
 const findProject = document.getElementById("find-project")
+const logo = document.getElementById("logo")
 
 
 function deleteInitialAndContinue() {
-    initialContact.remove()
+    initialContact.style.display = "none"
     //show search project
     findProject.style.display = "block"
 }
 
+function goBackToInitial() {
+    findProject.style.display = "none"
+    initialContact.style.display = "block"
+}
+
 initialButton.addEventListener("click", deleteInitialAndContinue)
+logo.addEventListener("click", goBackToInitial)
