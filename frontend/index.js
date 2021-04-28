bd0b66a80ac81b2f2a19eee61132cdf567f51845
Eva <yi.zheng1709@gmail.com>
2021-04-28 14:23:49 -0400

    creates a div that will appear after the initial-contact div is removed

--- frontend/index.js
+++ frontend/index.js
@@ -1,9 +1,12 @@
 const initialButton = document.getElementById("initial-button")
 const initialContact = document.getElementById("initial-contact")
+const findProject = document.getElementById("find-project")
 
 
 function deleteInitialAndContinue() {
     initialContact.remove()
+    //show search project
+    findProject.style.display = "block"
 }
 
 initialButton.addEventListener("click", deleteInitialAndContinue)
