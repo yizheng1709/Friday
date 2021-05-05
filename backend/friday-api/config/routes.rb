5b7e246608cc5ab80fe7528ce76f1cd7fb158549
Eva <yi.zheng1709@gmail.com>
2021-05-05 02:01:04 -0400

    uses only keyword in routes to limit routes

--- backend/friday-api/config/routes.rb
+++ backend/friday-api/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :projects do 
-    resources :tasks
+  resources :projects, only: [:create, :show, :index] do 
+    resources :tasks, only: [:create, :index, :update]
   end 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
