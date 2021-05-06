d10d72f9ab58b9061071dd9caba366d4c1c49c23
Eva <yi.zheng1709@gmail.com>
2021-05-06 12:22:35 -0400

    moves newProjectForm to static project function

--- frontend/project.js
+++ frontend/project.js
@@ -19,5 +19,41 @@ class Project {
         `
       }
 
+    static newProjectForm() {
+        removeChildrenFromMain()
+        mainContainer.innerHTML += `
+        <div class="creating-project-div center responsive shadow" id="creating-project-div">
+              <form id="creating-project-form">
+                <br>
+                <label class="project-name label-font">Project Name</label><br><br>
+                <input type="text" class="creating-project-input" value=" "><br><br>
+                <label class="project-name label-font">Due Date</label><br><br>
+                <input type="date" class="creating-project-input" value="2022-05-05"><br><br>
+                <label class="project-name label-font">Group Supervisor's E-mail</label><br><br>
+                <input type="text" class="creating-project-input" value=" "><br><br>
+                <div id="group-members">
+                <label class="project-name label-font">Group Member's E-mail</label><br><br>
+                <input type="text" class="creating-project-input" value=" "><br><br>
+                <label class="project-name label-font">Task</label><br><br>
+                <input type="text" class="creating-project-input task-input" value=" "><br><br>
+                <label class="project-name label-font">Group Member's E-mail</label><br><br>
+                <input type="text" class="creating-project-input" value=" "><br><br>
+                <label class="project-name label-font">Task</label><br><br>
+                <input type="text" class="creating-project-input task-input" value=" "><br>
+                </div>
+                <br><br>
+                <input type="submit" class="initial-button bold" value="Next">
+                <br><br><br>
+              </form>
+              <button class="initial-button bold" id="add-another-member">Add Another Group Member</button>
+                <br><br><br>
+            </div><br><br><br>`
+        homeButton()
+        const creatingProjectForm = document.getElementById("creating-project-form")
+        const addAnotherMember = document.getElementById("add-another-member")
+        creatingProjectForm.addEventListener("submit", submitProject)
+        addAnotherMember.addEventListener("click", Project.addAnotherMemberInput)
+      }  
+
 }
 
